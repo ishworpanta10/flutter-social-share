a08565a50f980f69a60ee11a5b13cd3a4d1caac8
ishworpanta10 <ishworpanta10@gmail.com>
2020-07-18 19:48:14 +0545

    Getting data from firebase storage

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:social_share/widgets/header.dart';
+
+final _userRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -7,6 +10,51 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  void initState() {
+    super.initState();
+    getUsers();
+    // getUserById();
+  }
+
+  // get single user by its id
+
+  // getUserById() async {
+  //   final id = 'jI7S4ODSYzRsFJBIaA1p';
+  //   DocumentSnapshot doc = await _userRef.document(id).get();
+  //   print(doc.data);
+  //
+  // }
+
+  // getUsers() {
+  //   _userRef.getDocuments().then((QuerySnapshot snapshots) {
+  //     snapshots.documents.forEach((DocumentSnapshot snapshot) {
+  //       print(snapshot.data);
+  //
+  //     });
+  //   });
+  // }
+
+  //alternative using future instead of .then to get all data of collection
+
+  // getUsers() async {
+  //   final snapshots = await _userRef.getDocuments();
+  //   final snapshot = snapshots.documents;
+  //   snapshot.forEach((doc) {
+  //     print(doc.data);
+  //   });
+  // }
+
+  //using filter with where args
+  getUsers() async {
+    final snapshots =
+        await _userRef.where("isAdmin", isEqualTo: true).getDocuments();
+    final snapshot = snapshots.documents;
+    snapshot.forEach((doc) {
+      print(doc.data);
+    });
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
